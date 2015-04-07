91522c802c24329a50daa978f2e39a88509ba97e
Roman Sklyarov <sorx00@gmail.com>
2015-04-08 02:03:56 +0300

    Use blkid instead of vol_id; Fixed identation; More verbosity

--- isotostick.sh
+++ isotostick.sh
@@ -21,216 +21,230 @@
 export PATH=/sbin:/usr/sbin:$PATH
 
 usage() {
-    echo "$0 [--reset-mbr] [--noverify] <isopath> <usbstick device>"
-    exit 1
+  #echo "$0 [--reset-mbr] [--noverify] <isopath> <usbstick device>"
+  echo "$0 [--reset-mbr] <isopath> <usbstick device>"
+  exit 1
 }
 
 cleanup() {
-    [ -d "$CDMNT" ] && umount $CDMNT && rmdir $CDMNT
-    [ -d "$USBMNT" ] && umount $USBMNT && rmdir $USBMNT
+  [ -d "$CDMNT" ] && umount $CDMNT && rmdir $CDMNT
+  [ -d "$USBMNT" ] && umount $USBMNT && rmdir $USBMNT
 }
 
 exitclean() {
-    echo "Cleaning up to exit..."
-    cleanup
-    exit 1
+  echo "Cleaning up to exit..."
+  cleanup
+  exit 1
 }
 
 getdisk() {
-    DEV=$1
+  DEV=$1
 
-    p=$(udevadm info --query=path --name $DEV)
-    if [ -e /sys/$p/device ]; then
-	device=$(basename /sys/$p)
-    else
-	device=$(basename $(readlink -f /sys/$p/../))
-    fi
-    if [ ! -e /sys/block/$device -o ! -e /dev/$device ]; then
-	echo "Error finding block device of $DEV.  Aborting!"
-	exitclean
-    fi
+  p=$(udevadm info --query=path --name $DEV)
+  if [ -e /sys/$p/device ]; then
+  device=$(basename /sys/$p)
+  else
+  device=$(basename $(readlink -f /sys/$p/../))
+  fi
+  if [ ! -e /sys/block/$device -o ! -e /dev/$device ]; then
+  echo "Error finding block device of $DEV.  Aborting!"
+  exitclean
+  fi
 
-    device="/dev/$device"
+  device="/dev/$device"
 }
 
 resetMBR() {
-    getdisk $1
-    cat /usr/lib/syslinux/mbr.bin > $device
+  getdisk $1
+  cat /usr/lib/syslinux/mbr.bin > $device
 }
 
 checkMBR() {
-    getdisk $1
+  getdisk $1
 
-    bs=$(mktemp /tmp/bs.XXXXXX)
-    dd if=$device of=$bs bs=512 count=1 2>/dev/null || exit 2
-    
-    mbrword=$(hexdump -n 2 $bs |head -n 1|awk {'print $2;'})
-    rm -f $bs
-    if [ "$mbrword" = "0000" ]; then
-	echo "MBR appears to be blank."
-	echo "Do you want to replace the MBR on this device?"
-	echo "Press Enter to continue or ctrl-c to abort"
-	read
-	resetMBR $1
-    fi
+  bs=$(mktemp /tmp/bs.XXXXXX)
+  dd if=$device of=$bs bs=512 count=1 2>/dev/null || exit 2
+  
+  mbrword=$(hexdump -n 2 $bs |head -n 1|awk {'print $2;'})
+  rm -f $bs
+  if [ "$mbrword" = "0000" ]; then
+  echo "MBR appears to be blank."
+  echo "Do you want to replace the MBR on this device?"
+  echo "Press Enter to continue or ctrl-c to abort"
+  read
+  resetMBR $1
+  fi
 
-    return 0
+  return 0
 }
 
 checkPartActive() {
-    dev=$1
-    getdisk $dev
-    
-    # if we're installing to whole-disk and not a partition, then we 
-    # don't need to worry about being active
-    if [ "$dev" = "$device" ]; then
-	return
-    fi
+  dev=$1
+  getdisk $dev
+  
+  # if we're installing to whole-disk and not a partition, then we 
+  # don't need to worry about being active
+  if [ "$dev" = "$device" ]; then
+  return
+  fi
 
-    if [ "$(/sbin/fdisk -l $device 2>/dev/null |grep $dev |awk {'print $2;'})" != "*" ]; then
-	echo "Partition isn't marked bootable!"
-	echo "You can mark the partition as bootable with "
-        echo "    # /sbin/parted $device"
-	echo "    (parted) toggle N boot"
-	echo "    (parted) quit"
-	exitclean
-    fi
+  if [ "$(/sbin/fdisk -l $device 2>/dev/null |grep $dev |awk {'print $2;'})" != "*" ]; then
+  echo "Partition isn't marked bootable!"
+  echo "You can mark the partition as bootable with "
+    echo "    # /sbin/parted $device"
+  echo "    (parted) toggle N boot"
+  echo "    (parted) quit"
+  exitclean
+  fi
 }
 
 checkFilesystem() {
-    dev=$1
+  local part=$1
 
-    USBFS=$(/lib/udev/vol_id -t $dev)
-    if [ "$USBFS" != "vfat" -a "$USBFS" != "msdos" -a "$USBFS" != "ext2" -a "$USBFS" != "ext3" ]; then
-	echo "USB filesystem must be vfat or ext[23]"
-	exitclean
-    fi
+  echo -n "Get filesystem of USB partition... "
+  USBFS=$( /sbin/blkid -o value -s TYPE $part )
+  echo $USBFS
+  if [ "$USBFS" != "vfat" -a "$USBFS" != "msdos" -a "$USBFS" != "ext2" -a "$USBFS" != "ext3" ]; then
+    echo "USB filesystem must be vfat or ext[23]"
+    exitclean
+  fi
 
-    USBLABEL=$(/lib/udev/vol_id -u $dev)
-    if [ -n "$USBLABEL" ]; then 
-	USBLABEL="UUID=$USBLABEL" ; 
-    else
-	USBLABEL=$(/lib/udev/vol_id -l $dev)
-	if [ -n "$USBLABEL" ]; then 
-	    USBLABEL="LABEL=$USBLABEL" 
-	else
-	    echo "Need to have a filesystem label or UUID for your USB device"
-	    if [ "$USBFS" = "vfat" -o "$USBFS" = "msdos" ]; then
-		echo "Label can be set with /sbin/dosfslabel"
-	    elif [ "$USBFS" = "ext2" -o "$USBFS" = "ext3" ]; then
-		echo "Label can be set with /sbin/e2label"
-	    fi
-	    exitclean
-	fi
-    fi
+  echo -n "Try to get UUID of USB partition... "
+  USBLABEL=$( /sbin/blkid -o export -s UUID $part )
+  echo $USBLABEL
+  if [ -n "$USBLABEL" ]; then
+    return $?
+  fi
+
+  echo -n "Try to get LABEL of USB partition... "
+  USBLABEL=$( /sbin/blkid -o export -s LABEL $part )
+  echo $USBLABEL
+  if [ -n "$USBLABEL" ]; then 
+    return $?
+  fi
+
+  echo "Need to have a filesystem label or UUID for your USB device"
+  if [ "$USBFS" = "vfat" -o "$USBFS" = "msdos" ]; then
+    echo "Label can be set with /sbin/dosfslabel"
+  elif [ "$USBFS" = "ext2" -o "$USBFS" = "ext3" ]; then
+    echo "Label can be set with /sbin/e2label"
+  fi
+  exitclean
 }
 
 checkSyslinuxVersion() {
-    if [ ! -x /usr/bin/syslinux ]; then
-	echo "You need to have syslinux installed to run this script"
-	exit 1
-    fi
-    if ! syslinux 2>&1 | grep -qe -d; then
-	SYSLINUXPATH=""
-    else
-	SYSLINUXPATH="syslinux"
-	#menu texts are trimmed when syslinux is not in / no idea why
-	SYSLINUXPATH=""
-    fi
+  if [ ! -x /usr/bin/syslinux ]; then
+    echo "You need to have syslinux installed to run this script"
+    exit 1
+  fi
+
+  if ! syslinux 2>&1 | grep -qe -d; then
+    SYSLINUXPATH=""
+  else
+    SYSLINUXPATH="syslinux"
+    #menu texts are trimmed when syslinux is not in / no idea why
+    SYSLINUXPATH=""
+  fi
 }
 
+
+
+
 if [ $(id -u) != 0 ]; then 
-    echo "You need to be root to run this script"
-    exit 1
+  echo "You need to be root to run this script"
+  exit 1
 fi
 
+# Getting flags from command line
 while [ $# -gt 2 ]; do
-    case $1 in
-	--noverify)
-	    noverify=1
-	    ;;
-	--reset-mbr|--resetmbr)
-	    resetmbr=1
-	    ;;
-	*)
-	    usage
-	    ;;
-    esac
-    shift
+  case $1 in
+  --noverify)
+    noverify=1
+    ;;
+  --reset-mbr|--resetmbr)
+    resetmbr=1
+    ;;
+  *)
+    usage
+    ;;
+  esac
+  shift
 done
 
-ISO=$1
-USBDEV=$2
+# Global variables
+ISO=$1      # ISO image file
+USBPART=$2  # partition on USB device
+#USBDEV
 
-if [ -z "$ISO" -o ! -e "$ISO" ]; then
-    usage
+if [ -z "$ISO" -o ! -r "$ISO" -o -d "$ISO" ]; then
+  usage
 fi
 
-if [ -z "$USBDEV" -o ! -b "$USBDEV" ]; then
-    usage
+if [ -z "$USBPART" -o ! -b "$USBPART" ]; then
+  usage
 fi
 
-if [ -z "$noverify" ]; then
-    # verify the image
-    echo "Not verifying image...(no checkisomd5 in Ubuntu so skipping)!"
-    #checkisomd5 --verbose $ISO
-    if [ $? -ne 0 ]; then
-	echo "Are you SURE you want to continue?"
-	echo "Press Enter to continue or ctrl-c to abort"
-	read
-    fi
-fi
+echo "ISO image verifying is temporarily disabled due to absence of checkisomd5 in Ubuntu"
+#if [ -z "$noverify" ]; then
+#  # verify the image
+#  #checkisomd5 --verbose $ISO
+#  if [ $? -ne 0 ]; then
+#    echo "Are you SURE you want to continue?"
+#    echo "Press Enter to continue or ctrl-c to abort"
+#    read
+#  fi
+#fi
 
 # do some basic sanity checks.  
-checkSyslinuxVersion 
-checkFilesystem $USBDEV
-checkPartActive $USBDEV
-checkMBR $USBDEV
-[ -n $resetmbr ] && resetMBR $USBDEV
+checkSyslinuxVersion
+checkFilesystem $USBPART
+checkPartActive $USBPART
+checkMBR $USBPART
+[ -n $resetmbr ] && resetMBR $USBPART
 
 # FIXME: would be better if we had better mountpoints
 CDMNT=$(mktemp -d /media/cdtmp.XXXXXX)
 mount -o loop $ISO $CDMNT || exitclean
 USBMNT=$(mktemp -d /media/usbdev.XXXXXX)
-mount $USBDEV $USBMNT || exitclean
+mount $USBPART $USBMNT || exitclean
 
 trap exitclean SIGINT SIGTERM
 
 if [ -d $USBMNT/casper ]; then
-    echo "Already set up as live image.  Deleting old in fifteen seconds..."
-    sleep 15
+  echo "Already set up as live image.  Deleting old in fifteen seconds..."
+  sleep 15
 
-    rm -rf $USBMNT/casper
+  rm -rf $USBMNT/casper
 fi
 
 echo "Copying live image to USB stick"
 if [ ! -d $USBMNT/$SYSLINUXPATH ]; then mkdir $USBMNT/$SYSLINUXPATH ; fi
 
 for file in \
-	 README.diskdefines md5sum.txt \
-	.disk casper pool dists preseed install;do
-	cp -a $CDMNT/$file $USBMNT/
+   README.diskdefines md5sum.txt \
+  .disk casper pool dists preseed install;do
+  cp -a $CDMNT/$file $USBMNT/
 done
 
 cp $CDMNT/isolinux/* $USBMNT/$SYSLINUXPATH
 
 echo "Installing boot loader"
 if [ "$USBFS" = "vfat" -o "$USBFS" = "msdos" ]; then
-    # syslinux expects the config to be named syslinux.cfg 
-    # and has to run with the file system unmounted
-    mv $USBMNT/$SYSLINUXPATH/isolinux.cfg $USBMNT/$SYSLINUXPATH/syslinux.cfg
-    cleanup
-    if [ -n "$SYSLINUXPATH" ]; then
-	syslinux -d $SYSLINUXPATH $USBDEV
-    else
-	syslinux $USBDEV
-    fi
+  # syslinux expects the config to be named syslinux.cfg 
+  # and has to run with the file system unmounted
+  mv $USBMNT/$SYSLINUXPATH/isolinux.cfg $USBMNT/$SYSLINUXPATH/syslinux.cfg
+  cleanup
+  if [ -n "$SYSLINUXPATH" ]; then
+  syslinux -d $SYSLINUXPATH $USBPART
+  else
+  syslinux $USBPART
+  fi
 elif [ "$USBFS" = "ext2" -o "$USBFS" = "ext3" ]; then
-    # extlinux expects the config to be named extlinux.conf
-    # and has to be run with the file system mounted
-    mv $USBMNT/$SYSLINUXPATH/isolinux.cfg $USBMNT/$SYSLINUXPATH/extlinux.conf
-    extlinux -i $USBMNT/$SYSLINUXPATH
-    cleanup
+  # extlinux expects the config to be named extlinux.conf
+  # and has to be run with the file system mounted
+  mv $USBMNT/$SYSLINUXPATH/isolinux.cfg $USBMNT/$SYSLINUXPATH/extlinux.conf
+  extlinux -i $USBMNT/$SYSLINUXPATH
+  cleanup
 fi
 
 echo "USB stick set up as live image!"
